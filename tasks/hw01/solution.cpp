c5ac2ed1fc717ef00b00ceac4731030555153c9d
Vladimír Vávra <vavravl3@fit.cvut.cz>
2023-03-12 20:38:51 +0100

    hw01: Add file comparison method

--- tasks/hw01/solution.cpp
+++ tasks/hw01/solution.cpp
@@ -13,21 +13,36 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
-bool utf8ToFibonacci(const char *inFile, const char *outFile) {
-  return false;
-}
-
-bool fibonacciToUtf8(const char *inFile, const char *outFile) {
-  return false;
-}
-
 #ifndef __PROGTEST__
+const int BUFFER_SIZE = 4096;
 
 bool identicalFiles(const char *file1, const char *file2) {
-  return false;
+  ifstream f1(file1, ios::binary | ios::ate);
+  ifstream f2(file2, ios::binary | ios::ate);
+  if (f1.fail() || f2.fail() || f1.tellg() != f2.tellg()) {
+    return false;
+  }
+  f1.seekg(0, ios::beg);
+  f2.seekg(0, ios::beg);
+  char buffer1[BUFFER_SIZE];
+  char buffer2[BUFFER_SIZE];
+  while (f1.good() && f2.good()) {
+    f1.read(buffer1, BUFFER_SIZE);
+    f2.read(buffer2, BUFFER_SIZE);
+    if (f1.gcount() != f2.gcount() || memcmp(buffer1, buffer2, f1.gcount()) != 0) {
+      return false;
+    }
+  }
+  if (!f1.eof() || !f2.eof()) {
+    return false;
+  }
+  return true;
 }
 
 int main(int argc, char *argv[]) {
+  ifstream f1("example/dst_0.fib", ios::binary);
+  assert(identicalFiles("example/dst_0.fib", "example/dst_0.fib"));
+  assert(!identicalFiles("example/dst_0.fib", "example/dst_1.fib"));
   assert(utf8ToFibonacci("example/src_0.utf8", "output.fib") && identicalFiles("output.fib", "example/dst_0.fib"));
   assert(utf8ToFibonacci("example/src_1.utf8", "output.fib") && identicalFiles("output.fib", "example/dst_1.fib"));
   assert(utf8ToFibonacci("example/src_2.utf8", "output.fib") && identicalFiles("output.fib", "example/dst_2.fib"));
