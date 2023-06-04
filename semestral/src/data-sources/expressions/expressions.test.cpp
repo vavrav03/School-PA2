5abb782c5fc29523a032f0d41882d07aac5b2737
Vladimír Vávra <vavravl3@fit.cvut.cz>
2023-06-04 22:38:03 +0200

    sem: Add tests for projection

--- semestral/src/data-sources/expressions/expressions.test.cpp
+++ semestral/src/data-sources/expressions/expressions.test.cpp
@@ -13,8 +13,17 @@ void testProjection() {
   headers[1] = "age";
   auto wrapper = make_shared<DataSourceExpressionWrapper>(dataSource, "a");
   ProjectionExpression projection(wrapper, headers, "b");
-  cout << projection.toSQL() << endl;
   assert(toLowerCase(projection.toSQL()) == "select height,age from (select * from a) as b");
+  assert(projection.getHeaderSize() == 2);
+  assert(projection.hasNextRow());
+  vector<string> rows = projection.getNextRow();
+  assert(projection.getHeaderIndex("height") == 0);
+  assert(projection.getHeaderIndex("age") == 1);
+  assert(projection.getHeaderName(0) == "height");
+  assert(projection.getHeaderName(1) == "age");
+  assert(rows[0] == "180");
+  assert(rows[1] == "25");
+  assert(!projection.hasNextRow());
 }
 
 void testExpression() {
