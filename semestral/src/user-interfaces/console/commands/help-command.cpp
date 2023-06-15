5b70bbe068112c9eeabf9efd1f242b02bbb0459d
Vladimír Vávra <vavravl3@fit.cvut.cz>
2023-06-15 13:36:48 +0200

    sem: Improve help

--- semestral/src/user-interfaces/console/commands/help-command.cpp
+++ semestral/src/user-interfaces/console/commands/help-command.cpp
@@ -10,33 +10,35 @@ bool HelpCommand::matchesSyntactically(std::vector<Token> command) {
 
 // @formatter:off
 void HelpCommand::run(std::vector<Token> command) {
-  cout << "App specific commands" << endl;
-  cout << "   - help: Prints this help." << endl;
-  cout << "   - exit: Exits the program." << endl;
-  cout << "   - import: Imports a file to a new or existing variable" << endl;
-  cout << "       - <variable_name> = import \"<file_name>.csv\" returns CSV datasource and sets it to a <variable_name> variable" << endl;
-  cout << "       - <variable_name> = import \"<file_name>.json\" returns JSON datasource and sets it to a <variable_name> variable" << endl;
-  cout << "       - variable name must be a quoted path to a file relative to a current working directory" << endl;
-  cout << "   - export ... to ...: Creates a file filled by a content of a variable" << endl;
-  cout << "       - export <variable_name> to \"<file_name>.csv\" exports a content of a <variable_name> variable to a CSV file" << endl;
-  cout << "       - export <variable_name> to \"<file_name>.json\" exports a content of a <variable_name> variable to a JSON file" << endl;
-  cout << "   - vars: Prints a list of all variables" << endl;
-  cout << "   - print: Prints a content of a variable to console" << endl;
-  cout << "other operations follow standard relational algebra syntax:" << endl;
-  cout << "   - set-set operations: union \"∪\", intersect \"∩\", except \"\\\", cross \"×\", divide \"÷\"" << endl;
-  cout << "   - set-boolean operations: is_subset \"⊆\", equals \"=\", is_proper_subset \"⊂\"" << endl;
-  cout << "   - general joins: join \"[rel1.col1 < rel2.col3]\", left_semi_join \"<condition]\", right_semi_join \"[condition>\", left_anti_join \"!<condition]\", right_anti_join \"![condition>\"" << endl;
-  cout << "   - natural joins (same order as general joins): \"*\", \"<*\", \"*>\", \"!<*\", \"!*>\"" << endl;
-  cout << "   - boolean operations: and \"∧\", or \"∨\", not \"¬\"" << endl;
-  cout << "   - projection: project \"relation[col1, col3]\"" << endl;
-  cout << "   - selection: select \"(condition)\"" << endl;
-  cout << "   - rename: rename \"relation[col1 -> new_col1, col3 -> new_col3]\"" << endl;
-  cout << "General syntax rules:" << endl;
-  cout << "   - all operations are case insensitive" << endl;
-  cout << "   - \"(\" \")\" can be used to redefine priority" << endl;
-  cout << "   - no implicit operator priorities are implemented. You have to explicitly define priority by using \"(\" \")\". Left to right evaluation is used" << endl;
-  cout << "   - write date in format \"YYYY-MM-DD\" to ensure correct comparison" << endl;
-  cout << "   - write time in format \"HH:MM:SS\" to ensure correct comparison" << endl;
-  cout << "   - write timestamp in format \"YYYY-MM-DD HH:MM:SS\" to ensure correct comparison" << endl;
+  cout << R"(App specific commands)" << endl;
+  cout << R"(   - exit: Exits the program.)" << endl;
+  cout << R"(   - export ... to ...: Creates a file filled by a content of a variable)" << endl;
+  cout << R"(       - export <variable_name> to "<file_name>.csv" exports a content of a <variable_name> variable to a CSV file)" << endl;
+  cout << R"(       - export <variable_name> to "<file_name>.json" exports a content of a <variable_name> variable to a JSON file)" << endl;
+  cout << R"(   - help: Prints this help.)" << endl;
+  cout << R"(   - print: prints a content of an attached data source plus its header to console)" << endl;
+  cout << R"(       - print <variable_name>)" << endl;
+  cout << R"(       - print <expression>)" << endl;
+  cout << R"(   - sequelize: Prints a content of a variable to console in a sequelize format)" << endl;
+  cout << R"(   - <variable name> = <expression>: Attaches data source to a given variable)" << endl;
+  cout << R"(       - <variable_name> = "<file_path>.csv" - attaches CSV file. Must be in quotes with .csv extension)" << endl;
+  cout << R"(       - <variable_name> = "<file_path>.json" returns JSON datasource and sets it to a <variable_name> variable)" << endl;
+  cout << R"(       - <variable_name> = <expression> - attaches result of an expression to a <variable_name> variable)" << endl;
+  cout << R"(   - vars: Prints a list of all variables)" << endl;
+  cout << R"(other operations follow standard relational algebra syntax:)" << endl;
+  cout << R"(   - set-set operations: union "∪", intersect "∩", except "\", cross "×", divide "÷")" << endl;
+  cout << R"(   - set-boolean operations: is_subset "⊆", equals "=", is_proper_subset "⊂")" << endl;
+  cout << R"(   - general joins: join "[rel1.col1 < rel2.col3]", left_semi_join "<condition]", right_semi_join "[condition>", left_anti_join "!<condition]", right_anti_join "![condition>")" << endl;
+  cout << R"(   - natural joins (same order as general joins): "*", "<*", "*>", "!<*", "!*>")" << endl;
+  cout << R"(   - boolean operations: and "∧", or "∨", not "¬")" << endl;
+  cout << R"(   - projection: project "relation[col1, col3]")" << endl;
+  cout << R"(   - selection: select "(condition)" << endl;
+  cout << R"(   - rename: rename "relation[col1 -> new_col1, col3 -> new_col3]")" << endl;
+  cout << R"(General syntax rules:)" << endl;
+  cout << R"(   - all operations are case insensitive)" << endl;
+  cout << R"(   - {} can be used to redefine priority for relational expressions and () for boolean expressions)" << endl;
+  cout << R"(   - implicit operator priorities are implemented. Left to right evaluation is used)" << endl;
+  cout << R"(   - use quotes when dealing with file names. Otherwise, they are not going to be considered file names)" << endl;
+  cout << R"(   - write date in format "YYYY-MM-DD")" << endl;
 }
 // @formatter:on
